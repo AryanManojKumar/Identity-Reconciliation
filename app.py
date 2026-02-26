8b0f20c0cf6a1239a499ec462709a3784fd2dea7
AryanManojKumar <aryan9265778358@gmail.com>
2026-02-26 15:39:11 +0530

    docker_deployment

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ def health():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    import os
+    port = int(os.getenv("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
